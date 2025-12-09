bcb4b263fb2bd11be61d67000acd394864943912
Emmlass <enaibreemma@gmail.com>
2025-12-09 19:31:13 +0100

    Completing the header of Ndigo Travels

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
       <header className='flex justify-between gap-2 p-4'>
         <a href="" className="logo flex gap-2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 -rotate-90 w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-</svg>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+          </svg>
 
                
         <span className='font-bold text-xl'>Ndigo Travels</span>
@@ -24,13 +24,18 @@ function App() {
         </svg>
           </button>
         </div>
-       <div className="flex border gap-2 border-gray-300 rounded-full py-2 px-4">
+       <div className="flex border gap-2 border-gray-300 rounded-full py-2 px-4 items-center shadow-md shadow-gray-400">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-</svg>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+        <div className='bg-gray-500 rounded-full text-white border border-gray-500 overflow-hidden'>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 relative top-1">
+          <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
+        </svg>
 
-
-       </div>
+        </div>
+        </div>
+        
       </header>
     </>
   )
